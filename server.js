4b5281f704566e03dde1166db27defa25c87a63f
Ladydee3 <delisiletwala62@gmail.com>
2024-12-10 12:21:01 -0800

    task2

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+// server.js
 const express = require('express');
-const routes = require('./routes/index');
+const routes = require('./routes');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+// Use the routes defined in routes/index.js
 app.use('/', routes);
 
-const PORT = process.env.PORT || 1234;
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
+
